5b4c5d6aed0096e0a05496f4b57c914cf910dfb8
ignazka <picastle@posteo.de>
2026-04-20 20:11:02 +0200

    chore: formatting and minor style tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
-import { Noto_Sans, Geist_Mono } from "next/font/google";
-import { cn } from "@/lib/utils";
+import { Noto_Sans, Geist_Mono } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
-const geistMonoHeading = Geist_Mono({subsets:['latin'],variable:'--font-heading'});
+const geistMonoHeading = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-heading',
+});
 
-const notoSans = Noto_Sans({subsets:['latin'],variable:'--font-sans'});
-
+const notoSans = Noto_Sans({ subsets: ['latin'], variable: '--font-sans' });
 
 export default function RootLayout({
   children,
@@ -14,7 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning className={cn("font-sans", notoSans.variable, geistMonoHeading.variable)}>
+    <html
+      lang='en'
+      suppressHydrationWarning
+      className={cn('font-sans', notoSans.variable, geistMonoHeading.variable)}
+    >
       <body>
         <ThemeProvider attribute='class' defaultTheme='dark' forcedTheme='dark'>
           {children}
